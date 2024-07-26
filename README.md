fb9ccc3608f42bc21848e31e80a9ac7aae4949d3
David Aragundy <136662251+waldaara@users.noreply.github.com>
2024-07-26 17:36:55 -0500

    Infra/db (#2)
    * ⚗️ Test db

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 ## Aciertamente
+
+To run this project, you need to have Bun installed on your machine and a Neon Project.
+
+```bash
+git clone
+cd aciertamente
+```
+
+Before running the project, you need to install the dependencies.
+
+```bash
+bun install
+```
+
+Now, you need to set the environment variables. You can find the environment variables in the `.env.example` file. Create a new file called `.env.local` and copy the content from the `.env.example` file. Fill in the values with your own credentials of your **development** branch.
+
+```bash
+cp .env.example .env.local
+```
+
+Now, you can run the project.
+
+```bash
+bun run dev
+```
+
+The project will be running on `http://localhost:3000`.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,144 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { db, user } from "@/drizzle";
+import { NeonDbError } from "@neondatabase/serverless";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
-export default function Home() {
+type User = typeof user.$inferInsert;
+
+export const revalidate = 0;
+
+export default async function Home() {
+  const users: User[] = await db.select().from(user);
+
+  users.unshift({
+    id: 0,
+    name: "",
+    email: "",
+    password: "",
+    role: "admin",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
       <h1 className="text-4xl font-bold">Welcome to Aciertamente</h1>
-      <Button>Click me</Button>
+
+      <form
+        action={async (formData: FormData) => {
+          "use server";
+
+          const name = formData.get("name") as string;
+          const email = formData.get("email") as string;
+          const password = formData.get("password") as string;
+          const role = formData.get("role") as "admin" | "customer";
+
+          if (!name || !email || !password || !role) return;
+
+          try {
+            await db.insert(user).values({
+              name,
+              email,
+              password,
+              role,
+              createdAt: new Date(),
+            });
+          } catch (error) {
+            if (error instanceof NeonDbError) console.error(error.detail);
+          }
+
+          redirect("/");
+        }}
+        className="flex flex-col justify-center gap-4"
+      >
+        <p>
+          <label>
+            Name
+            <Input name="name" required />
+          </label>
+        </p>
+
+        <p>
+          <label>
+            Email
+            <Input name="email" type="email" required />
+          </label>
+        </p>
+
+        <p>
+          <label>
+            Password
+            <Input name="password" type="password" required />
+          </label>
+        </p>
+
+        <p>
+          <label>
+            Role
+            <Select name="role" required>
+              <SelectTrigger>
+                <SelectValue placeholder="Select a role" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="admin">Admin</SelectItem>
+                  <SelectItem value="customer">Customer</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </label>
+        </p>
+
+        <Button type="submit" title="Create user">
+          Create user
+        </Button>
+      </form>
+
+      <h2 className="text-2xl font-bold">Users</h2>
+
+      <div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              {Object.keys(users[0]).map((key) => (
+                <TableHead key={key}>{key}</TableHead>
+              ))}
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {users.slice(1).map((user) => (
+              <TableRow key={user.id}>
+                <TableCell>{user.id}</TableCell>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>{user.password}</TableCell>
+                <TableCell>{user.role}</TableCell>
+                <TableCell>{user.createdAt.toDateString()}</TableCell>
+                <TableCell>{user.updatedAt?.toDateString()}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </main>
   );
 }
