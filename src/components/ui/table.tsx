872a58d9122e05b6a164bfee50bc378d1260a271
David Aragundy <136662251+waldaara@users.noreply.github.com>
2025-06-16 18:53:16 +0000

    fix: add header to table

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -11,7 +11,12 @@ const Table = React.forwardRef<
       ref={ref}
       className={cn("w-full caption-bottom text-sm", className)}
       {...props}
-    />
+    >
+      <tr>
+        <th></th>
+        <th></th>
+      </tr>
+    </table>
   </div>
 ));
 Table.displayName = "Table";
