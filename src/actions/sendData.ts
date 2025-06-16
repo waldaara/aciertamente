339ee09ad095e51e4799bd599a69984e45797097
David Aragundy <136662251+waldaara@users.noreply.github.com>
2025-06-16 18:44:48 +0000

    fix: better error handling

--- src/actions/sendData.ts
+++ src/actions/sendData.ts
@@ -26,6 +26,6 @@ export async function sendData(formData: FormData) {
       sex,
     });
   } catch (error) {
-    console.log("Failed to send data!!!!")
+    console.log(`Exception while sending data: ${err}`);
   }
 }
